f45713fd7395b1509547dcfbf69646b44095462a
George Crary <craryg@onid.oregonstate.edu>
2018-02-25 21:58:20 -0800

    Initial mapper alongside Gramsci test texts

---
+++ mapper.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env/python
+
+import sys
+
+def mapper():
+	dict = {}
+	for line in sys.stdin:
+		print "DEBUG {0}".format(line)
+		for word in line.split(" "):
+			if word not in dict:
+				dict[word] = 1;
+			else:
+				dict[word] = dict[word] + 1;
+	for (key, val) in dict.items():
+		print "{0}\t{1}".format(key, val)
+
+if __name__ == "__main__":
+	mapper()
